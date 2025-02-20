41f18300530043ad400ab911bee00499c06ec458
Rodrigo Pinto <pintorodrigo2007@gmail.com>
2025-02-20 15:45:10 +0000

    ConexaoDB

---
+++ WebAlunos/Models/ConexaoBD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAlunos.Models
+{
+    public class ConexaoBD
+    {
+        private string host;
+        private int porta;
+        private string bd;
+        private string utilizador;
+        private string password;
+
+        private ConexaoBD(string host, int porta, string bd, string utilizador, string password) {
+            this.host = host;
+            this.porta = porta;
+            this.bd = bd;
+            this.utilizador = utilizador;
+            this.password = password;
+        }
+    }
+}
