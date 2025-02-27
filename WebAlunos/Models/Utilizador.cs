6fe8f5e6273b91b5456fc93320fac1f923c352d8
Rodrigo Pinto <pintorodrigo2007@gmail.com>
2025-02-27 15:16:49 +0000

    up

---
+++ WebAlunos/Models/Utilizador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAlunos.Models
+{
+    public class Utilizador
+    {
+        public int NUtilizador {  get; }
+        [Display(Name = "E-Mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
